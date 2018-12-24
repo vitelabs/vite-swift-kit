745f63723a70d06a05a81fb78ff83519a5d843e0
Stone <stone.hsia@gmail.com>
2018-12-24 11:16:47 +0800

    fix

--- ViteWallet/Classes/Const/ViteError.swift
+++ ViteWallet/Classes/Const/ViteError.swift
@@ -13,12 +13,12 @@ import JSONRPCKit
 public struct ViteError: Error {
 
     public let code: ViteErrorCode
-    public let message: String
+    public let rawMessage: String
     public let rawError: Error?
 
-    public init(code: ViteErrorCode, message: String, rawError: Error?) {
+    public init(code: ViteErrorCode, rawMessage: String, rawError: Error?) {
         self.code = code
-        self.message = "\(message)(\(code.toString()))"
+        self.rawMessage = "\(rawMessage)(\(code.toString()))"
         self.rawError = rawError
     }
 }
@@ -42,66 +42,66 @@ extension ViteError {
 
             var rawError: NSError!
             var code = ViteErrorCode(type: .custom, id: 0)
-            var message = ""
+            var rawMessage = ""
 
             switch error {
             case .connectionError(let e):
                 rawError = (e as NSError)
                 code = ViteErrorCode(type: .st_con, id: rawError.code)
-                message = rawError.localizedDescription
+                rawMessage = rawError.localizedDescription
             case .requestError(let e):
                 rawError = (e as NSError)
                 code = ViteErrorCode(type: .st_req, id: rawError.code)
-                message = rawError.localizedDescription
+                rawMessage = rawError.localizedDescription
             case .responseError(let e):
                 if let rpcError = e as? JSONRPCError {
                     return conversionJSONRPCError(from: rpcError)
                 } else {
                     rawError = (e as NSError)
                     code = ViteErrorCode(type: .st_res, id: rawError.code)
-                    message = rawError.localizedDescription
+                    rawMessage = rawError.localizedDescription
                 }
             }
-            return ViteError(code: code, message: message, rawError: rawError)
+            return ViteError(code: code, rawMessage: rawMessage, rawError: rawError)
         } else {
-            return ViteError(code: ViteErrorCode(type: .custom, id: (error as NSError).code), message: (error as NSError).localizedDescription, rawError: error)
+            return ViteError(code: ViteErrorCode(type: .custom, id: (error as NSError).code), rawMessage: (error as NSError).localizedDescription, rawError: error)
         }
     }
 
     fileprivate static func conversionJSONRPCError(from error: JSONRPCError) -> ViteError {
         var rawError = (error as NSError)
         var code = ViteErrorCode(type: .custom, id: 0)
-        var message = ""
+        var rawMessage = ""
 
         switch error {
         case .responseError(let c, let m, _):
             code = ViteErrorCode(type: .rpc, id: c)
-            message = m
+            rawMessage = m
         case .responseNotFound:
             code = ViteErrorCode(type: .rpc_res_nf, id: 0)
-            message = "responseNotFound"
+            rawMessage = "responseNotFound"
         case .resultObjectParseError(let e):
             rawError = (e as NSError)
             code = ViteErrorCode(type: .rpc_ro_p, id: rawError.code)
-            message = "resultObjectParseError \(rawError.localizedDescription)"
+            rawMessage = "resultObjectParseError \(rawError.localizedDescription)"
         case .errorObjectParseError(let e):
             rawError = (e as NSError)
             code = ViteErrorCode(type: .rpc_eo_p, id: rawError.code)
-            message = "errorObjectParseError \(rawError.localizedDescription)"
+            rawMessage = "errorObjectParseError \(rawError.localizedDescription)"
         case .unsupportedVersion(let str):
             code = ViteErrorCode(type: .rpc_u_v, id: 0)
-            message = "unsupportedVersion \(str ?? "")"
+            rawMessage = "unsupportedVersion \(str ?? "")"
         case .unexpectedTypeObject:
             code = ViteErrorCode(type: .rpc_u_t, id: 0)
-            message = "unexpectedTypeObject"
+            rawMessage = "unexpectedTypeObject"
         case .missingBothResultAndError:
             code = ViteErrorCode(type: .rpc_m_re, id: 0)
-            message = "missingBothResultAndError"
+            rawMessage = "missingBothResultAndError"
         case .nonArrayResponse:
             code = ViteErrorCode(type: .rpc_nar, id: 0)
-            message = "nonArrayResponse"
+            rawMessage = "nonArrayResponse"
         }
 
-        return ViteError(code: code, message: message, rawError: rawError)
+        return ViteError(code: code, rawMessage: rawMessage, rawError: rawError)
     }
 }
--- ViteWallet/Classes/Const/ViteErrorCode.swift
+++ ViteWallet/Classes/Const/ViteErrorCode.swift
@@ -63,11 +63,11 @@ public struct ViteErrorCode {
 
 extension ViteError {
     public static var JSONTypeError: ViteError {
-        return ViteError(code: ViteErrorCode.JSONType, message: "JSON Type Error", rawError: nil)
+        return ViteError(code: ViteErrorCode.JSONType, rawMessage: "JSON Type Error", rawError: nil)
     }
 
     public static var cancelError: ViteError {
-        return ViteError(code: ViteErrorCode.cancel, message: "Cancel Operation", rawError: nil)
+        return ViteError(code: ViteErrorCode.cancel, rawMessage: "Cancel Operation", rawError: nil)
     }
 }
 
