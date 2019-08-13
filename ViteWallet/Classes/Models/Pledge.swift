e90cf2a1f5a4567f5bf6e540907f6a7c94cf4fb4
Stone <stone.hsia@gmail.com>
2019-08-13 15:25:22 +0800

    pledge add property

--- ViteWallet/Classes/Models/Pledge.swift
+++ ViteWallet/Classes/Models/Pledge.swift
@@ -15,7 +15,11 @@ public struct Pledge: Mappable {
     public fileprivate(set) var beneficialAddress = ""
     public fileprivate(set) var amount = Amount(0)
     public fileprivate(set) var timestamp = Date(timeIntervalSince1970: 0)
-    public fileprivate(set) var withdrawHeight = Amount(0)
+    public fileprivate(set) var withdrawHeight: UInt64 = 0
+
+    public fileprivate(set) var agent = false
+    public fileprivate(set) var agentAddress = ""
+    public fileprivate(set) var bid: UInt8 = 0
 
     public init?(map: Map) { }
 
@@ -23,6 +27,10 @@ public struct Pledge: Mappable {
         beneficialAddress <- map["beneficialAddr"]
         amount <- (map["amount"], JSONTransformer.balance)
         timestamp <- (map["withdrawTime"], JSONTransformer.timestamp)
-        withdrawHeight <- (map["withdrawHeight"], JSONTransformer.bigint)
+        withdrawHeight <- (map["withdrawHeight"], JSONTransformer.uint64)
+
+        agent <- map["agent"]
+        agentAddress <- map["agentAddress"]
+        bid <- map["bid"]
     }
 }
